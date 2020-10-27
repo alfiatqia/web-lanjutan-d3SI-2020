be4701ae05bd0f843741870c849bc2100123ebdd
alfiatqia <fiyanichan2612@gmail.com>
2020-10-26 23:02:18 -0700

    praktikum boostrap 27/10/2020

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -1,18 +1,27 @@
-<?php
+<?php 
 
 /**
- * 
- */
-class mahasiswa extends CI_Controller
-{
+  * 
+  */
+ class mahasiswa extends CI_Controller
+ {
+
+ 	
+ 	public function index ($url_nim, $url_jur)
+ 	{
+ 		# code...
+ 		$data ['nama']= 'Alfi atqia';
+ 		$data ['umur']= '21';
+ 		$data ['nim']= $url_nim;
+ 		$data ['jurusan']= $url_jur;
 
 
+ 		$this->load->view('mahasiswa/V_index',$data); 
+ 	}
 
-
-	public function jurusan()
-	{
-		echo "d3 sistem informasi";
-	}
-
-}
-?>
+ 	public function jurusan ()
+ 	{
+ 		echo 'd3 sistem informasi';
+ 	}
+ }
+  ?>
---
+++ application/views/mahasiswa/v_index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>DATA MAHASISWA</title>
+	<link rel="stylesheet" 
+	 	  href="<?= base_url('assets/boostrap/css/boostrap.min.css')?>">
+
+</head>
+
+<body>
+
+	<h2>DATA MAHASISWA</h2>
+	<p>nama : <?php echo $nama;  ?></p>
+	<p>umur : <?php echo $umur; ?> </p>
+	<p>NIM : <?php echo $nim;  ?> </p>
+	<p>jurusan : <?php echo $jurusan;  ?></p>
+
+</body>
+
+</html>
